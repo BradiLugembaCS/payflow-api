8ed8580f895805b3c9daeada2272e45a2fd2adf5
BradiLugembaTUD <124157741+BradiLugembaTUD@users.noreply.github.com>
2026-09-08 16:53:51 +0100

    Create users table for account data

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,18 +4,32 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
+
+	"github.com/BradiLugembaCS/payflow-api/internal/database"
 	"github.com/BradiLugembaCS/payflow-api/internal/server"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal("database connection failed: ", err)
+	}
+	defer db.Close()
+
+	log.Println("Connected to PostgreSQL")
+
 	srv := server.New()
 
-	addr := ":8080"
+	addr := "127.0.0.1:8080"
 
 	log.Printf("PayFlow API starting on %s", addr)
 
-	err := http.ListenAndServe(addr, srv.Handler())
-	if err != nil {
+	if err := http.ListenAndServe(addr, srv.Handler()); err != nil {
 		log.Fatal(err)
 	}
 }
